9d971c4ecf1111114e4bc9f715d5321d4c6f43b4
Tom Cahill <me@tomcahill.co.uk>
2022-10-27 15:51:39 +0100

    ADDED: Subs to ltnService

--- src/LtnService.ts
+++ src/LtnService.ts
@@ -1,3 +1,5 @@
+import { v4 } from 'uuid';
+
 import { LtnElement } from './LtnElement.js';
 
 export interface LtnTraderService {
@@ -5,4 +7,31 @@ export interface LtnTraderService {
   service: LtnElement;
 }
 
-export class LtnService extends LtnElement {}
+export class LtnService extends LtnElement {
+
+  protected _subscriptions: {[index: string]: {event: string, cb: Function}} = {};
+
+  // eslint-disable-next-line class-methods-use-this
+  static generateId(): string {
+    return v4();
+  }
+
+  eventSubscribe(event: string, cb: Function) {
+    const id = LtnService.generateId();
+    this._subscriptions[id] = {event, cb};
+    return id;
+  }
+
+  eventUnsubscribe(id: string): void {
+    if (this._subscriptions[id]) delete this._subscriptions[id];
+  }
+
+  // eslint-disable-next-line no-undef
+  dispatchCustomEvent(eventName: string, options: CustomEventInit) {
+    this.dispatchEvent(new CustomEvent(eventName, options));
+
+    Object.values(this._subscriptions)
+      .filter((sub) => sub.event === eventName)
+      .forEach((sub) => sub.cb(options.detail));
+  }
+}
