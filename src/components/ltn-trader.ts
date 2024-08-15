36e683646f53fadfcb55c77597e1dd69a0b78548
Tom Cahill <me@tomcahill.co.uk>
2024-08-15 10:06:25 +0100

    ADDED: License stuff

--- src/components/ltn-trader.ts
+++ src/components/ltn-trader.ts
@@ -1,3 +1,19 @@
+/**
+ * Ltn Element
+ * Copyright (C) 2016-2024 Data People Connected LTD.
+ * <https://www.dpc-ltd.com/>
+ *
+ * This file is part of Ltn Element.
+ * Ltn Element is free software: you can redistribute it and/or modify it under the
+ * terms of the GNU Affero General Public Licence as published by the Free Software
+ * Foundation, either version 3 of the Licence, or (at your option) any later version.
+ * Ltn Element is distributed in the hope that it will be useful, but WITHOUT ANY WARRANTY;
+ * without even the implied warranty of MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.
+ * See the GNU Affero General Public Licence for more details.
+ * You should have received a copy of the GNU Affero General Public Licence along with
+ * this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+
 import { LtnTrader } from '../LtnTrader.js';
 
 customElements.define('ltn-trader', LtnTrader);
